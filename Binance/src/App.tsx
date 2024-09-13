70de12c981ce9f06b176705e7a19e2c397e3db12
Your Name <mjahdkaran123@gmail.com>
2024-09-13 17:24:11 +0330

    set responsive style

--- Binance/src/App.tsx
+++ Binance/src/App.tsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import Header from './Components/Header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <></>
+    <>
+      <header className='Header_holder'>
+        <div className='desktop'> <Header />   </div>
+
+        <div className='mobile'><Navbar /></div>
+
+      </header>
+
+    </>
   )
 }
 
